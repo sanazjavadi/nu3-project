16062b5728bd0c4368ccf9cba433e392557ecc6b
sanaz.javadi <sanaz.javadi@zoodfood.com>
2022-07-05 09:03:37 +0430

    handle axios base url condition

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 export const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL:
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : "https://nu3-project.vercel.app/",
 });
 
 // Now all requests using this instance will wait 2.5 seconds before timing out
